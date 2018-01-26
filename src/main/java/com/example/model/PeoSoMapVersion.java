59919a736790e3512a3d6808aef3be1b94702301
Văn Bằng <congbang10h@gmail.com>
2018-01-26 19:49:12 +0700

    update name package

--- src/main/java/com/example/model/PeoSoMapVersion.java
+++ src/main/java/com/example/model/PeoSoMapVersion.java
@@ -1,5 +1,5 @@
-// default package
-// Generated Jan 26, 2018 7:38:22 PM by Hibernate Tools 4.3.5.Final
+package com.example.model;
+// Generated Jan 26, 2018 7:48:52 PM by Hibernate Tools 4.3.5.Final
 
 import java.util.Date;
 import java.util.HashSet;
